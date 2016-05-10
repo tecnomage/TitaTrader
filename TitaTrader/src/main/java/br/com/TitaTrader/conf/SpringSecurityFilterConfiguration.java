f1971b1c5455adccd7487e187d0595e79bb95f2c
vinicius <vinicius@vinicius-System-Product-Name>
2016-05-09 21:43:57 -0300

    commit inicial do projeto

---
+++ TitaTrader/src/main/java/br/com/TitaTrader/conf/SpringSecurityFilterConfiguration.java
@@ -0,0 +1,8 @@
+package br.com.TitaTrader.conf;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer
+{
+
+}
